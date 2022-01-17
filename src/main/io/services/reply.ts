752e4edc19b00523e6573cc8a82f164bda56c2fa
wallycoding <wallysonbreno.dev@gmail.com>
2022-01-17 16:31:10 -0300

    correction reply

--- src/main/io/services/reply.ts
+++ src/main/io/services/reply.ts
@@ -4,7 +4,7 @@ const createReply = (socket: SocketIo) => {
 
     const replyController = {
         done(event: string, data?: any) {
-            socket.emit(event, null, data);
+            socket.emit(`${event}-response`, null, data);
         },
         error(event: string, error: any) {
             socket.emit(`${event}-response`, error)
